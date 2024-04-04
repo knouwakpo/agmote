6d889a6be9f9a3c85d16999f8e146b08227c36bd
knouwakpo <163920793+knouwakpo@users.noreply.github.com>
2024-04-04 10:35:00 -0600

    Delete notecard/Save_tempfile_signal.py

--- notecard/Save_tempfile_signal.py
+++
@@ -1,9 +0,0 @@
-import pigpio
-
-mypigpio = pigpio.pi()
-
-#Triggers an event to signal to the background data
-# processing program to save the radiodata.txt file to 
-# radiodata.temp.x.txt file (x ranges from 0 to 255).
-#These files will be read, and the data sent to notehub
-mypigpio.event_trigger(3)
